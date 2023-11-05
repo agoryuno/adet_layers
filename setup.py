fdfc05f229a6119bf9cdb6a7b3a1032b6ed2b981
Alexander Goryunov <alex.goryunov@gmail.com>
2023-11-05 16:17:33 +0200

    update path

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ assert torch_ver >= [1, 3], "Requires PyTorch >= 1.3"
 
 def get_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "adet_layers", "csrc")
+    extensions_dir = os.path.join(this_dir, "adet_layers")
 
     main_source = os.path.join(extensions_dir, "vision.cpp")
     sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
